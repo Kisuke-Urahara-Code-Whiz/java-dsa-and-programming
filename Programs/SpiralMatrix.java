ed77dc3a0629edf9741be642484ef352c39fe448
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-10-15 02:24:18 +0530

    spiral matrix solved wohoo

---
+++ Programs/SpiralMatrix.java
@@ -0,0 +1,62 @@
+package Programs;
+
+public class SpiralMatrix {
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] spiral = new int[n][n];
+        right(spiral, 0, 0, 0, n-1 , 0 , n-1, 1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(spiral[i][j]+ "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void right(int[][] spiral, int m, int n, int r1, int r2, int c1, int c2, int a){
+        if(spiral[m][n]!=0)
+            return;
+        else{
+            while(n<=c2){
+                spiral[m][n++] = a++;
+            }
+            down(spiral,m+1,n-1,r1+1,r2,c1,c2,a);
+        }
+    }
+
+    public static void down(int[][] spiral, int m, int n, int r1, int r2, int c1, int c2, int a){
+        if(spiral[m][n]!=0)
+            return;
+        else{
+            while(m<=r2){
+                spiral[m++][n] = a++;
+            }
+            left(spiral,m-1,n-1,r1,r2,c1,c2-1,a);
+        }
+    }
+
+    public static void left(int[][] spiral, int m, int n, int r1, int r2, int c1, int c2, int a){
+        if(spiral[m][n]!=0)
+            return;
+        else{
+            while(n>=c1){
+                spiral[m][n--] = a++;
+            }
+            up(spiral, m-1, n+1, r1, r2-1, c1, c2, a);
+        }
+    }
+
+    public static void up(int[][] spiral, int m, int n, int r1, int r2, int c1, int c2, int a){
+        if(spiral[m][n]!=0)
+            return;
+        else{
+            while(m>=r1){
+                spiral[m--][n] = a++;
+            }
+            right(spiral, m+1, n+1, r1, r2, c1+1, c2, a);
+        }
+    }
+
+
+}
