33935b1a2c36b7caee670bebad3bdec485d01452
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-10-15 01:03:04 +0530

    Update LeetCode61.java

--- LeetCodeMedium/LeetCode61.java
+++ LeetCodeMedium/LeetCode61.java
@@ -5,28 +5,30 @@ import Programs.ListNode;
 public class LeetCode61 extends ListNode {
 
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode temp = head;
-        ListNode last = head;
-        int l = 1;
-        while(last.next!=null){
-            last = last.next;
-            l+=1;
-        }
-        l = l - (k%l);
-        if(l!=0) {
-            boolean flag = true;
-            while (flag) {
-                if (l - 1 == 0)
-                    flag = false;
-                else {
-                    temp = temp.next;
-                    l -= 1;
-                }
+        if(head!=null) {
+            ListNode temp = head;
+            ListNode last = head;
+            int l = 1;
+            while (last.next != null) {
+                last = last.next;
+                l += 1;
             }
-            ListNode temp1 = head;
-            last.next = temp1;
-            head = temp.next;
-            temp.next = null;
+            l = l - (k % l);
+            if (l != 0) {
+                boolean flag = true;
+                while (flag) {
+                    if (l - 1 == 0)
+                        flag = false;
+                    else {
+                        temp = temp.next;
+                        l -= 1;
+                    }
+                }
+                ListNode temp1 = head;
+                last.next = temp1;
+                head = temp.next;
+                temp.next = null;
+            }
         }
         return head;
     };
