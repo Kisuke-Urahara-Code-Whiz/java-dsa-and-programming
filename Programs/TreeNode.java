e53997e0ecdb41765bce3bfb5641154ca06b30f2
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-10-27 01:45:20 +0530

    Update TreeNode.java

--- Programs/TreeNode.java
+++ Programs/TreeNode.java
@@ -1,5 +1,7 @@
 package Programs;
 
+import java.util.*;
+
 public class TreeNode {
 
     public int val;
@@ -11,6 +13,33 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public TreeNode buildTree(Integer[] arr) {
+        if (arr.length == 0 || arr[0] == null) return null;
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (i < arr.length) {
+            TreeNode current = queue.poll();
+
+            if (i < arr.length && arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+
+        return root;
     }
 
 }
