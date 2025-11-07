028ddd9f9b3feee5fe6c4b707d72da2bcd46e267
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-11-07 19:30:01 +0530

    Update AVLTest.java

--- Programs/AVLTest.java
+++ Programs/AVLTest.java
@@ -4,14 +4,16 @@ public class AVLTest {
 
     public static void main(String[] args) {
         AVL tree = new AVL();
-        tree.insert(5);
-        tree.inorder();
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(7);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(60);
+        tree.insert(80);
         tree.insert(10);
-        tree.insert(8);
-        tree.insert(3);
+        tree.insert(25);
+        tree.insert(35);
+        tree.insert(45);
         tree.preorder();
         tree.preorderStack();
     }
