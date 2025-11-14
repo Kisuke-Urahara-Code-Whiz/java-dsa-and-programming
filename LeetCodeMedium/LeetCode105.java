13471c5fb0c7add14f6568077783ffe11287a28b
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-11-14 20:25:10 +0530

    Update LeetCode105.java

--- LeetCodeMedium/LeetCode105.java
+++ LeetCodeMedium/LeetCode105.java
@@ -4,36 +4,32 @@ import Programs.TreeNode;
 
 public class LeetCode105  extends TreeNode {
 
-    int p3 = 0;
+    int x = 0;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int p1 = 0;
-        int p2 = 0;
+        return build(preorder,inorder,0,inorder.length-1);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int p, int q){
         boolean flag = true;
-        while(p2 < inorder.length && flag){
-            if(inorder[p2] == preorder[p3])
+        int a = p;
+        while(a<=q && flag){
+            if(preorder[x]==inorder[a])
                 flag = false;
             else
-                p2+=1;
+                a+=1;
         }
         if(!flag){
-            TreeNode curr = new TreeNode(preorder[p3]);
-            int[] arr1 = new int[p2];
-            int[] arr2 = new int[inorder.length - p2 - 1];
-            for(int i =p1;i<arr1.length;i++){
-                arr1[p1++] = inorder[i];
+            TreeNode curr = new TreeNode(preorder[x]);
+            int r = a-1;
+            int l = a+1;
+            if(r>=p) {
+                x+=1;
+                curr.left = build(preorder, inorder, p, r);
             }
-            p1+=1;
-            for(int i=0;i< arr2.length;i++){
-                arr2[i] = inorder[p1++];
-            }
-            if(arr1.length>0){
-                p3+=1;
-                curr.left = buildTree(preorder,arr1);
-            }
-            if(arr2.length>0){
-                p3+=1;
-                curr.right = buildTree(preorder,arr2);
+            if(l<=q){
+                x+=1;
+                curr.right = build(preorder, inorder, l, q);
             }
             return curr;
         }
