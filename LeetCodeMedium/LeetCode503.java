2421e9a2afcd4ada8a0dbbaef793eb6c520b5e76
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-11-24 09:22:02 +0530

    corrected leetcode503

--- LeetCodeMedium/LeetCode503.java
+++ LeetCodeMedium/LeetCode503.java
@@ -8,6 +8,7 @@ public class LeetCode503 {
     public int[] nextGreaterElements(int[] nums) {
         Deque<Integer> deque = new ArrayDeque<>();
         int i = 0;
+        int first = nums[i];
         deque.push(i++);
         while(i<nums.length){
             while(!deque.isEmpty() && nums[deque.peek()]<nums[i]){
@@ -15,15 +16,41 @@ public class LeetCode503 {
             }
             deque.push(i++);
         }
+
         while(!deque.isEmpty()){
             int index = deque.pop();
             if(!deque.isEmpty()){
-                nums[index] = nums[deque.getLast()]>nums[index]?nums[deque.getLast()]:-1;
-            } else {
+                boolean flag = false;
+                if(nums[index]<first) {
+                    nums[index] = first;
+                    flag = true;
+                }
+                if(!flag) {
+                    for (int j = 0; j <= deque.getLast(); j++) {
+                        if (nums[j] > nums[index]) {
+                            nums[index] = nums[j];
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) nums[index] = -1;
+                }
+            } else
                 nums[index] = -1;
-            }
         }
         return nums;
     }
 
+    public static void main(String[] args) {
+        new LeetCode503().nextGreaterElements(new int[]{1,2,3,2,1});
+    }
 }
+
+//        while(!deque.isEmpty()){
+//            int index = deque.pop();
+//            if(!deque.isEmpty()){
+//                nums[index] = nums[deque.getLast()]>nums[index]?nums[deque.getLast()]:-1;
+//            } else {
+//                nums[index] = -1;
+//            }
+//        }
