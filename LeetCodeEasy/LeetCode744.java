1a8ab4222e8d737827dfac967dff8637da9dcea8
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-01-05 13:01:29 +0530

    resolved leetcode 744

--- LeetCodeEasy/LeetCode744.java
+++ LeetCodeEasy/LeetCode744.java
@@ -2,20 +2,19 @@ package LeetCodeEasy;
 
 class LeetCode744 {
     public char nextGreatestLetter(char[] letters, char target) {
-        int l = 0, r = letters.length - 1;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (letters[mid] <= target) {
-                l = mid + 1;
+        int l = 0;
+        int r = letters.length-1;
+        char result = letters[0];
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(letters[mid]>target){
+                result = letters[mid];
+                r = mid-1;
             } else {
-                r = mid - 1;
+                l = mid+1;
             }
         }
-
-        // if no letter greater than target, wrap around
-        return letters[l % letters.length];
+        return result;
     }
 }
 
