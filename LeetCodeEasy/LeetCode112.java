ecf9a91a1a2725239769e1dceb99058a4c5e3dcd
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-01-14 09:52:01 +0530

    resolved leetcode112

--- LeetCodeEasy/LeetCode112.java
+++ LeetCodeEasy/LeetCode112.java
@@ -5,25 +5,13 @@ import Programs.TreeNode;
 public class LeetCode112 extends TreeNode {
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root!=null){
-            boolean flag = root.left==null && root.right==null && root.val==targetSum;
-            if(!flag) {
-                flag = sum(root.left, targetSum, root.val);
-                flag = flag || sum(root.right, targetSum, root.val);
-            }
+        if(root==null) return false;
+        else if(targetSum-root.val==0 && root.left==null && root.right==null) return true;
+        else{
+            boolean flag = false;
+            if(root.left!=null) flag=hasPathSum(root.left,targetSum-root.val);
+            if(root.right!=null && !flag) flag=hasPathSum(root.right, targetSum-root.val);
             return flag;
-        }
-        return false;
-    }
-
-    public boolean sum(TreeNode root, int targetSum, int sum) {
-        if(root!=null){
-            sum = sum+root.val;
-            boolean flag = root.left!=null?sum(root.left,targetSum,sum): root.right == null && sum == targetSum;
-            flag = flag || (root.right != null && sum(root.right, targetSum,sum));
-            return flag;
-        } else {
-            return false;
         }
     }
 
