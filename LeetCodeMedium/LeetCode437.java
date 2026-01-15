e38a20f9aa51bc294d445232ec412022accaba5a
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-01-15 13:36:48 +0530

    wrote the optimal solution for leetcode437

--- LeetCodeMedium/LeetCode437.java
+++ LeetCodeMedium/LeetCode437.java
@@ -2,47 +2,47 @@ package LeetCodeMedium;
 
 import Programs.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class LeetCode437 extends TreeNode {
 
     int result = 0;
+    HashMap<Long, Integer> map = new HashMap<>();
+    List<Long> prefixSum = new ArrayList<>();
 
     public int pathSum(TreeNode root, int targetSum) {
         if(root==null) return 0;
-        Deque<Long> deque = new LinkedList<>();
-        findPaths(root, deque, (long)targetSum);
+        List<Long> path = new ArrayList<>();
+        prefixSumOps((long)0, -1, (long)targetSum);
+        preorder(root, 0, (long)targetSum);
         return result;
     }
 
-    private void findPaths(TreeNode node, Deque<Long> deque, long targetSum){
-        int size = deque.size();
-        while(size!=0){
-            long n = deque.remove();
-            n-=node.val;
-            if(n==0) result+=1;
-            deque.add(n);
-            size-=1;
+    private void preorder(TreeNode node, int index, long targetSum){
+        if(node.left==null && node.right==null){
+            prefixSumOps((long)node.val, index, targetSum);
+            map.put(prefixSum.getLast(), map.get(prefixSum.getLast())-1);
+            prefixSum.removeLast();
+        } else {
+            prefixSumOps((long)node.val, index, targetSum);
+            if(node.left!=null) preorder(node.left, index+1, targetSum);
+            if(node.right!=null) preorder(node.right, index+1, targetSum);
+            map.put(prefixSum.getLast(), map.get(prefixSum.getLast())-1);
+            prefixSum.removeLast();
         }
-        long val = targetSum-node.val;
-        if(val==0) result+=1;
-        deque.add(val);
-        if(node.left!=null) findPaths(node.left, deque, targetSum);
-        if(node.right!=null) findPaths(node.right, deque, targetSum);
-        deque.removeLast();
-        size = deque.size();
-        while(size!=0){
-            deque.add(deque.remove()+ node.val);
-            size-=1;
-        }
+    }
+
+    private void prefixSumOps(long l, int index, long targetSum){
+        if(index==-1) prefixSum.add(l);
+        else prefixSum.add(prefixSum.get(index)+l);
+        long needed = prefixSum.getLast()-targetSum;
+        result+=map.getOrDefault(needed,0);
+        map.put(prefixSum.getLast(),map.getOrDefault(prefixSum.getLast(),0)+1);
     }
 
     static void main(){
         LeetCode437 obj = new LeetCode437();
-        TreeNode root = obj.buildTree(new Integer[]{1000000000,1000000000,null,294967296,null,1000000000,null,1000000000,null,1000000000});
-        System.out.println("Answer -> "+obj.pathSum(root,0));
+        TreeNode root = obj.buildTree(new Integer[]{10,5,-3,3,2,null,11,3,-2,null,1});
+        System.out.println("Answer -> "+obj.pathSum(root,8));
     }
 }
