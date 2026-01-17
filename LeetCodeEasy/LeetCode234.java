e8985935d1295114970715103329e8d7c56ebf6d
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-01-17 12:33:35 +0530

    solved leetcode234 2 more times

--- LeetCodeEasy/LeetCode234.java
+++ LeetCodeEasy/LeetCode234.java
@@ -5,14 +5,39 @@ import Programs.ListNode;
 public class LeetCode234 {
 
     public boolean isPalindrome(ListNode head) {
-//        ListNode node = head;
-//        int length = -1;
-//        while(node!=null){
-//            length+=1;
-//            node = node.next;
-//        }
-//        if(length%2==0) length = (length/2)+1;
-//        else length = length%2;
+        if(head.next==null) return true;
+        else if(head.next.next==null) return head.val==head.next.val;
+        int length = -1;
+        ListNode node = head;
+        while(node!=null){
+            length+=1;
+            node = node.next;
+        }
+        length = length/2;
+        node = head;
+        while(length!=0){
+            node=node.next;
+            length-=1;
+        }
+        node.next = reverse(node.next);
+        node=node.next;
+        while(node!=null){
+            if(node.val!=head.val) return false;
+            node = node.next;
+            head = head.next;
+        }
         return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
     }
 }
