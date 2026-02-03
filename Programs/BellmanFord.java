bb4d6aa6a4ad8798d40b0aed6f5c37363208c62f
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-02-03 15:52:14 +0530

    Completed the Bellman Ford algorithm for one last time

--- Programs/BellmanFord.java
+++ Programs/BellmanFord.java
@@ -6,21 +6,16 @@ import java.util.List;
 //Perfect Implementation of Bellman Ford algorithm but the algorithm comes with a lot of tradeoffs
 //It keeps reducing the costs even though not needed till n-1 relaxations are complete
 //We are able to detect negative cycle detection at the nth relaxation
-//Point is -> Bellman Ford is the best single source shortest path algo for negative cycle detection
-//Not the best choice for find the single source shortest path algo for finding the shortest path when negative weights are involved
-//As it keeps reducing unnecessary weights when not needed leading to give us ambiguous shortest path which is extra and incorrect
+//Point is Bellman Ford is able to detect the shortest Path from a single source to all nodes if no negative weight is involved
+//Mathematically there is no way to detect the shortest path in case of a negative weight cycle
+//Thus, in my previous commits I have tried to trace the shortest distance and path, but now I will remove the code as it's not optimal
+
 public class BellmanFord {
 
-    private Result algorithm(int[][] edges, int n, int src){
-        int[] predecessors = new int[n];
-        int[] shortestPathCounts = new int[n];
+    private List<Integer> algorithm(int[][] edges, int n, int src){
         List<Integer> shortestDistances = new ArrayList<>();
         for(int i=0;i<n;i++){
-           int predecessor = -1;
-           int shortestPathCount = i==src?0:-1;
            Integer shortestDistance = i==src?0:null;
-           predecessors[i] = predecessor;
-           shortestPathCounts[i] = shortestPathCount;
            shortestDistances.add(shortestDistance);
         }
         for(int i=0;i<n-1;i++){
@@ -30,18 +25,27 @@ public class BellmanFord {
                     Integer prevShortestDistance = shortestDistances.get(j[1]);
                     if(prevShortestDistance==null || cost<prevShortestDistance){
                         shortestDistances.set(j[1], cost);
-                        shortestPathCounts[j[1]] = shortestPathCounts[j[0]]+1;
-                        predecessors[j[1]] = j[0];
-                    } else if(cost==prevShortestDistance){
-                        if(shortestPathCounts[j[0]]+1<shortestPathCounts[j[1]]){
-                            shortestPathCounts[j[1]] = shortestPathCounts[j[0]]+1;
-                            predecessors[j[1]] = j[0];
-                        }
                     }
                 }
             }
         }
-        return new Result(predecessors, shortestPathCounts, shortestDistances);
+
+        boolean negativeCycle = false;
+
+        for(int[] j : edges){
+            if(shortestDistances.get(j[0]) != null){
+
+                int cost = shortestDistances.get(j[0]) + j[2];
+                Integer prevShortestDistance = shortestDistances.get(j[1]);
+
+                if(prevShortestDistance == null || cost < prevShortestDistance){
+                    negativeCycle = true;
+                    break;
+                }
+            }
+        }
+
+        return shortestDistances;
     }
 
     public static void main(String[] args) {
@@ -70,23 +74,10 @@ public class BellmanFord {
         int n = 12;
         int src = 0;
         BellmanFord obj = new BellmanFord();
-        Result result = obj.algorithm(edges, n, src);
-        List<Integer> shortestDistances = result.getShortestDistances();
-        int[] predecessors = result.getPredecessors();
-        int[] shortestPathCounts = result.getShortestPathCounts();
+        List<Integer> result = obj.algorithm(edges, n, src);
         for(int i=0;i<n;i++){
-            System.out.println(src+" -> "+i+" , Shortest Distance -> "+shortestDistances.get(i)+" , Path = "+obj.getPath(shortestPathCounts[i], predecessors, i));
+            System.out.println(src+" -> "+i+" , Shortest Distance -> "+result.get(i));
         }
-    }
-
-    private String getPath(int pathCount, int[] predecessor, int destination){
-        int node = destination;
-        StringBuilder str = new StringBuilder();
-        while(node!=-1){
-            str.append(" <- ").append(node);
-            node = predecessor[node];
-        }
-        return new String(str).substring(3);
     }
 
 }
