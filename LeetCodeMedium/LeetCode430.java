128a708535377ab1a421e6b0df37182ca190a6aa
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-03-02 00:35:12 +0530

    wrote a new shorter code for the same problem

--- LeetCodeMedium/LeetCode430.java
+++ LeetCodeMedium/LeetCode430.java
@@ -10,27 +10,24 @@ public class LeetCode430 {
     };
 
     public Node flatten(Node head) {
-        if(head==null)
-            return null;
-        traversal(head);
-        return head;
+        if(head==null) return null;
+        Node result = new Node();
+        flatten(result, head);
+        result.next.prev = null;
+        result = result.next;
+        return result;
     }
 
-    private Node traversal(Node curr){
-        Node temp = curr;
-        while(curr!=null){
-            if(curr.child!=null){
-                temp = curr.next;
-                curr.next = curr.child;
-                curr.next.prev = curr;
-                curr.child = null;
-                curr = traversal(curr.next);
-                curr.next = temp;
-                if(temp!=null) temp.prev = curr;
-            }
-            temp = curr;
-            curr = curr.next;
-        }
-        return temp;
+    public Node flatten(Node prev, Node curr) {
+        if(curr!=null) {
+            prev.next = curr;
+            curr.prev = prev;
+            Node temp = curr.next;
+            Node returnValue = null;
+            if (curr.child != null) returnValue = flatten(flatten(curr, curr.child), temp);
+            else returnValue = flatten(curr, curr.next);
+            curr.child = null;
+            return returnValue;
+        } else return prev;
     }
 }
