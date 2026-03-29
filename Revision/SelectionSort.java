a0b8ea7da03611224d67532565e033597ba1d474
Sougata Mondal <sougatacodewhiz@gmail.com>
2026-03-29 19:29:00 +0530

    Create SelectionSort.java

---
+++ Revision/SelectionSort.java
@@ -0,0 +1,26 @@
+package Revision;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    private void sort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int max = 0;
+            for(int j=1;j<=arr.length-i-1;j++){
+                if(arr[j]>arr[max]) max=j;
+            }
+            int temp = arr[max];
+            arr[max] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        SelectionSort obj = new SelectionSort();
+        int[] arr = new int[]{5,4,3,2,1};
+        obj.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
